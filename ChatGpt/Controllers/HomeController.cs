2e8fde2e2f75cb432aefc99ade36395f66ed6934
Zeynep Esen <zeynepesen.insense@hotmail.com>
2023-11-30 14:35:58 +0300

    Add project files.

---
+++ ChatGpt/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
+
+namespace ChatGpt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> GetAIBasedResult(string SearchText)
+        {
+            //  string APIKey = "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";
+            string APIKey = "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";
+            string answer=string.Empty;
+            var openai=new OpenAIAPI(APIKey);
+            CompletionRequest completion=new CompletionRequest();
+            completion.Prompt = SearchText;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 900;
+            var result=openai.Completions.CreateCompletionAsync(completion);
+            foreach(var item in result.Result.Completions)
+            {
+                answer=item.Text;
+            }
+            return Ok(answer);
+        }
+    }
+}
